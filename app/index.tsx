84a4b6cfd2287fdad627997fdec0d5ad3c05d959
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-29 10:07:42 -0400

    Refactor Index component to display offers in a list
    - Replace static welcome message with a FlatList of offers
    - Implement conditional styling based on item index
    - Enhance item display with images and titles

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,88 @@
-import { Text, View } from 'react-native'
+import { images, offers } from '@/constants'
+import cn from 'clsx'
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Index() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-xl font-bold text-blue-500'>
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-white'>
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          // Check if item index is even or odd to apply different styles
+          const isEven = index % 2 === 0
+          const pressableConditionalClass = isEven
+            ? 'flex-row-reverse'
+            : 'flex-row'
+          const textInfoAlignment = isEven ? 'pl-10' : 'pr-10'
+
+          return (
+            <View key={index}>
+              <Pressable
+                className={cn('offer-card', pressableConditionalClass)}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: '#FFFFFF222' }}
+              >
+                {({ pressed }) => (
+                  <>
+                    {/* Item Image */}
+                    <View className='h-full w-1/2'>
+                      <Image
+                        source={item.image}
+                        className='size-full'
+                        resizeMode='cover'
+                      />
+                    </View>
+
+                    {/* Item Title */}
+                    <View className={cn('offer-card__info', textInfoAlignment)}>
+                      <Text className='h1-bold text-white leading-tight'>
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className='size-10'
+                        resizeMode='contain'
+                        tintColor='#FFF'
+                      />
+                    </View>
+                  </>
+                )}
+              </Pressable>
+            </View>
+          )
+        }}
+        contentContainerClassName='pb-28 px-5'
+        ListHeaderComponent={() => (
+          <View className='flex-between flex-row w-full my-5'>
+            <View className='flex-start'>
+              <Text className='small-bold text-primary uppercase'>
+                DELIVER TO
+              </Text>
+
+              <TouchableOpacity className='flex-center flex-row gap-x-1 mt-0.5'>
+                <Text className='paragraph-bold text-dark-100'>
+                  United States
+                </Text>
+                <Image
+                  source={images.arrowDown}
+                  className='size-3'
+                  resizeMode='contain'
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text>Cart</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   )
 }
